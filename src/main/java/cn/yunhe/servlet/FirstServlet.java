f165f9a2cd0002a3f1b5ed2e0a3e30dec485033a
ChianLiu <chianliu_015@163.com>
2019-09-29 16:09:06 +0800

    me

--- src/main/java/cn/yunhe/servlet/FirstServlet.java
+++ src/main/java/cn/yunhe/servlet/FirstServlet.java
@@ -25,7 +25,7 @@ public class FirstServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().print("hello maven and git and commit");
+		response.getWriter().print("hello maven and git and commit and gourp");
 	}
 
 	/**
